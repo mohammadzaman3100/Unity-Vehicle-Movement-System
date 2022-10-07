06a7dccfb262e5d4ec58672aea9abc2ff9d13c47
Mohammad Zaman <55985418+mohammadzaman3100@users.noreply.github.com>
2022-10-07 18:01:16 +0100

    adding the environment

--- Assets/Scripts/RacingCar.cs
+++ Assets/Scripts/RacingCar.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RacingCar : MonoBehaviour
 {
-        public float speed = 10.0f;
+    public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
     public AudioClip engineSound;
     public AudioClip doorSound;
